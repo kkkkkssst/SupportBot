01b7838a5cf1c933f2c9cf1baa38996a9b5d167a
kkkkkssst <kostiantyn.zhenylo@nure.ua>
2024-06-04 17:50:40 +0300

    Created methods for getting case from CRM and changed functional of case creating

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const axios = require('axios');
 const Cookies = require('js-cookie');
 const TelegramBot = require('node-telegram-bot-api');
+const path = require('path');
 
 const bot = new TelegramBot(process.env.BOTKEY, {
     polling: {
@@ -10,9 +11,14 @@ const bot = new TelegramBot(process.env.BOTKEY, {
         autoStart: true
     }
 });
+bot.removeAllListeners("message");
 const commands = [{
     command: "start",
     description: "Запуск бота💫"
+},
+{
+    command: "menu",
+    description: "Меню🔎"
 },
 {
     command: "cancel",
@@ -21,10 +27,9 @@ const commands = [{
 
 ];
 
-var userConnections = {};
-var cookies;
-var BPMCSRF;
-
+let userConnections = {};
+let cookies;
+let BPMCSRF;
 const setAuthorization = async () => {
     await authorization();
     setInterval(async () => {
@@ -41,15 +46,12 @@ const start = () => {
         bot.setMyCommands(commands);
         bot.on('text', async msg => {
             if (msg.text === "/start" || msg.text === "/start@CRM_Genesis_Support_bot") {
-
                 if (msg.chat.type === 'group') {
-                    //await bot.sendMessage(msg.chat.id, `Вітаю всіх учасників групи!👋🏻\nGroup Id ${msg.chat.id}`);
                     userConnections[msg.chat.id] = {
                         Id: msg.from.id
                     }
                     await bot.sendMessage(msg.chat.id, `Вітаю, ${msg.from.first_name} !👋🏻`);
-                }
-                else if (msg.chat.type === 'private') {
+                } else if (msg.chat.type === 'private') {
                     userConnections[msg.chat.id] = {
                         Id: msg.chat.id
                     }
@@ -57,7 +59,6 @@ const start = () => {
                 }
                 if (cookies && BPMCSRF) {
                     var authResult = await getShadowAuthData(userConnections[msg.chat.id]);
-
                     if (authResult === "access denied") {
                         await bot.sendMessage(msg.chat.id, 'Для авторизації надайте Ваш номер телефону 📲', {
                             reply_markup: {
@@ -74,26 +75,55 @@ const start = () => {
                                 keyboard: [
                                     ['Додати новий кейс🆕'],
                                     ['Мої кейси📎']
-                                ]
+                                ],
+                                resize_keyboard: true
                             }
-
                         });
                         userConnections[msg.chat.id].status = 1;
                     }
                 }
-            } else if (userConnections[msg.chat.id] && msg.text === "/cancel") {
+            } else if (msg.text === "/menu" || msg.text === "/menu@CRM_Genesis_Support_bot" && userConnections[msg.chat.id].status > 0) { 
+                await bot.sendMessage(msg.chat.id, "Меню🔎", {
+                    reply_markup: {
+                        keyboard: [
+                            ['Додати новий кейс🆕'],
+                            ['Мої кейси📎']
+                        ],
+                        resize_keyboard: true
+                    }
+                });
+                userConnections[msg.chat.id].status = 1;
+            } else if (!userConnections[msg.chat.id] && userConnections[msg.chat.id].status > 0) {
+                return;
+            } else if (msg.text === "/cancel") {
                 userConnections[msg.chat.id].status = 0;
                 await bot.sendMessage(msg.chat.id, "Дію скасовано♻️");
-            } else if (userConnections[msg.chat.id] && msg.text === "Додати новий кейс🆕") {
+            } else if (!userConnections[msg.chat.id].status) {
+                return;
+            } else if (userConnections[msg.chat.id].status === 1 && msg.text === "Додати новий кейс🆕") {
                 await bot.sendMessage(msg.chat.id, "Дайте назву зверненню 🖍", {
                     reply_markup: {
                         force_reply: true,
+                        remove_keyboard: true
                     }
                 });
                 userConnections[msg.chat.id].status = 2;
-            } else if (userConnections[msg.chat.id] && userConnections[msg.chat.id].status === 2) {
+            } else if (userConnections[msg.chat.id].status === 1 && msg.text === "Мої кейси📎") {
+                let caseStates = await getCaseStates();
+                let inline_keyboard = [];
+                caseStates.forEach(async (item) => {
+                    inline_keyboard.push([{ text: item, callback_data: item }]);
+                });
+                await bot.sendMessage(msg.chat.id, "Виберіть статус кейсу ♻️", {
+                    reply_markup: {
+                        inline_keyboard: inline_keyboard
+                    }
+                });
+                userConnections[msg.chat.id].status = 5;
+            } else if (userConnections[msg.chat.id].status === 2) {
                 userConnections[msg.chat.id].appeal = {
-                    subject: msg.text
+                    subject: msg.text,
+                    files: []
                 }
                 await bot.sendMessage(msg.chat.id, `Опишіть Вашу проблему🎯`, {
                     reply_markup: {
@@ -101,10 +131,10 @@ const start = () => {
                     }
                 });
                 userConnections[msg.chat.id].status = 3;
-            } else if (userConnections[msg.chat.id] && userConnections[msg.chat.id].status == 3) {
+            } else if (userConnections[msg.chat.id].status === 3) {
                 if (userConnections[msg.chat.id].appeal) {
                     userConnections[msg.chat.id].appeal.description = msg.text;
-                } 
+                }
 
                 await bot.sendMessage(msg.chat.id, `Чи потрібно додати до звернення файл(скріншот, документ, тощо)?📄`, {
                     reply_markup: JSON.stringify({
@@ -114,6 +144,9 @@ const start = () => {
                         ]
                     })
                 });
+            } else if (userConnections[msg.chat.id].status === 4) {
+                let appealNum = await createCase(userConnections[msg.chat.id]);
+                await bot.sendMessage(msg.chat.id, `Звернення успішно оформлено під номером ${appealNum}!✅ `);
             }
         });
         bot.on('contact', async msg => {
@@ -144,13 +177,14 @@ const start = () => {
                         keyboard: [
                             ['Додати новий кейс🆕']
                             ['Мої кейси📎']
-                        ]
+                        ],
+                        resize_keyboard: true
                     }
                 });
             }
         });
         bot.on('callback_query', async msg => {
-            if (!Object.keys(userConnections).length > 0) {
+            if (!userConnections[msg.message.chat.id] || !userConnections[msg.message.chat.id].status) {
                 return;
             }
             if (userConnections[msg.message.chat.id].status == 3 && msg.data == "AcceptAddingDocument") {
@@ -159,27 +193,77 @@ const start = () => {
                         force_reply: true
                     }
                 });
-                userConnections[msg.message.chat.id].status = 4;
+                //userConnections[msg.message.chat.id].status = 4;
             } else if (userConnections[msg.message.chat.id].status == 3 && msg.data == "DeclineAddingDocument") {
                 let appealNum = await createCase(userConnections[msg.message.chat.id]);
                 await bot.sendMessage(msg.message.chat.id, `Звернення успішно оформлено під номером ${appealNum}!✅ `);
+            } else if (userConnections[msg.message.chat.id].status == 5) {
+                let cases = await getUserCases(userConnections[msg.message.chat.id], msg.data);
+                let inline_keyboard = [];
+                cases.forEach(async (item) => {
+                    inline_keyboard.push([{ text: item, callback_data: item }]);
+                });
+                if (inline_keyboard.length > 0) {
+                    await bot.sendMessage(msg.message.chat.id, "Кейси зі статусом " + msg.data + " 🗓", {
+                        reply_markup: JSON.stringify({
+                            inline_keyboard: inline_keyboard
+                        })
+                    });
+                } else {
+                    await bot.sendMessage(msg.message.chat.id, "Кейси зі статусом " + msg.data + " відсутні ❌");
+                }
             }
 
         });
         bot.on(`document`, async msg => {
-            if (Object.keys(userConnections).length > 0) {
-                if (userConnections[msg.chat.id].status == 3) {
-                    await bot.sendMessage(msg.chat.id, `Документ прийнято!✅`);
-                    userConnections[msg.chat.id].appeal.file = {}
+            if (!userConnections[msg.chat.id] || !userConnections[msg.chat.id].status) {
+                return;
+            } else if (userConnections[msg.chat.id].status == 3) {
+                let file = {};
+                file.file_url = await bot.getFileLink(msg.document.file_id);
+                file.file_name = msg.document.file_name;
+                userConnections[msg.chat.id].appeal.files.push(file);
+                let simulatedMessage = {
+                    chat: {
+                        id: msg.chat.id,
+                    },
+                    text: 'ready',
+                };
+                if (!userConnections[msg.chat.id].appeal.documentFlag) {
+                    userConnections[msg.chat.id].appeal.documentFlag = true;
+                    setTimeout(function () {
+                        bot.emit('text', simulatedMessage);
+                    }, 3000);
+                    userConnections[msg.chat.id].status = 4;
+                }
 
-                    userConnections[msg.chat.id].appeal.file.file_url = await bot.getFileLink(msg.document.file_id);
-                    userConnections[msg.chat.id].appeal.file.file_name = msg.document.file_name;
+            }
 
-                    let appealNum = await createCase(userConnections[msg.chat.id]);
-                    await bot.sendMessage(msg.chat.id, `Звернення успішно оформлено під номером ${appealNum}!✅ `);
+        });
+        bot.on('photo', async msg => {
+            if (!userConnections[msg.chat.id] || !userConnections[msg.chat.id].status) {
+                return;
+            } else if (userConnections[msg.chat.id].status === 3) {
+                let file = {};
+                file.file_url = await bot.getFileLink(msg.photo[0].file_id);
+                file.file_name = path.basename(file.file_url);
+                userConnections[msg.chat.id].appeal.files.push(file);
 
+                let simulatedMessage = {
+                    chat: {
+                        id: msg.chat.id,
+                    },
+                    text: 'ready',
+                };
+                if (!userConnections[msg.chat.id].appeal.documentFlag) {
+                    userConnections[msg.chat.id].appeal.documentFlag = true;
+                    setTimeout(function () {
+                        bot.emit('text', simulatedMessage);
+                    }, 3000);
+                    userConnections[msg.chat.id].status = 4;
                 }
             }
+
         });
     } catch (error) {
         console.log(error);
@@ -187,7 +271,7 @@ const start = () => {
 }
 async function authorization() {
 
-    var loginUrl = process.env.LOGIN_URL;
+    var loginUrl = process.env.BASE_URL + process.env.LOGIN_URL;
     const authRequestBody = JSON.stringify({ UserName: process.env.LOGIN, UserPassword: process.env.PASSWORD });
 
     const requestOptions = {
@@ -297,7 +381,7 @@ async function setContactData(userConnection) {
 async function createCase(userConnection) {
     try {
         let url = "https://crmgenesis.creatio.com/0/rest/SupportBotService/CreateCase";
-        let requestBody = JSON.stringify({ description: userConnection.appeal.description, subject: userConnection.appeal.subject,telegramId: userConnection.Id, file: userConnection.appeal.file, source: "SupportBot"});
+        let requestBody = JSON.stringify({ description: userConnection.appeal.description, subject: userConnection.appeal.subject, telegramId: userConnection.Id, files: userConnection.appeal.files, source: "SupportBot" });
         let requestOptions = {
             method: 'POST',
             headers: {
@@ -315,6 +399,52 @@ async function createCase(userConnection) {
         console.log(err)
     }
 }
+async function getUserCases(userConnection, status) {
+    try {
+        let url = process.env.BASE_URL + process.env.SUPPORT_BOT_SERVICE_URL + "/GetUserCases";
+        let caseCollection = [];
+        let requestBody = JSON.stringify({ telegramId: userConnection.Id, caseStatus: status });
+        let requestOptions = {
+            method: 'POST',
+            headers: {
+                'Cookie': cookies.join('; '),
+                'BPMCSRF': BPMCSRF,
+                'Content-Type': 'application/json',
+            },
+            data: requestBody
+        }
+        let response = await axios.post(url, requestBody, requestOptions);
+        await response.data.forEach(async (item) => {
+            caseCollection.push(item);
+        });
+        return caseCollection;
+    } catch (err) {
+        console.log(err)
+    }
+}
+async function getCaseStates(userConnection) {
+    try {
+        let statesCollection = [];
+        let url = process.env.BASE_URL + "/0/odata/CaseStatus";
+        let requestOptions = {
+            method: 'GET',
+            headers: {
+                'Cookie': cookies.join('; '),
+                'BPMCSRF': BPMCSRF,
+                'Content-Type': 'application/json',
+            },
+
+        }
+        let response = await axios.get(url, requestOptions);
+        await response.data.value.forEach(async (item) => {
+            statesCollection.push(item.Name);
+        });
+        return statesCollection;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 
 setAuthorization();
 start();
